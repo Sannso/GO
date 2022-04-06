bf42bde16a5199af4ab3045f6b96e36bc6af832b
UTP <utp@utp.local>
2022-04-06 17:53:18 -0500

    first commite

---
+++ Semillero/class1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	a "fmt"
+)
+
+func main() {
+	a.Println("Hello world!")
+
+	//---------- Variables ------------------------
+	// var num int
+	// num = 0
+	// // Otra forma
+	// num2 := 3
+
+	// a.Println("Numeros: ", num, num2)
+
+	//----------- Slice -------------------
+	// vas slice = make(int[], num elementos, cap)
+
+	//---------- Funciones ---------------------
+
+	//------------- Ciclos ---------------------
+	// for j := 0; j < 10; j++ {}
+	// for ; j < 10 ; {} -> While
+	// for ; ; {} -> while (true)
+
+}
